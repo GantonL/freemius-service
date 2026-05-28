e2ad69bcb2a1826a1ccfe594b63c26bff3ec00f7
Guy Lahav <guylahav@machine.local>
2026-05-28 21:59:42 +0300

    feat: support checkout link endopint

--- src/modules/checkout/checkout.controller.ts
+++ src/modules/checkout/checkout.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post, UseGuard } from "@danet/core";
+import { Body, Controller, Get, Post, Query, UseGuard } from "@danet/core";
 import { FreemiusService } from "../freemius/freemius.service.ts";
 import { InternalNetworkRequestGuard } from "../../guards/internal-network-request.guard.ts";
 import { CheckoutValidateRequestDto } from "./dtos/checkout-validate-request.dto.ts";
+import { FreemiusSupportedCheckoutLanguages } from "../../types.ts";
 
 @Controller("checkout")
 @UseGuard(InternalNetworkRequestGuard)
@@ -25,5 +26,25 @@ export class CheckoutController {
       body.url,
     );
     return result;
+  }
+
+  @Get("link")
+  async getLink(
+    @Query("productId") productId: string,
+    @Query("planId") planId: string,
+    @Query("userEmail") userEmail: string,
+    @Query("readonlyUser") readonlyUser: boolean,
+    @Query("environment") environment: string,
+    @Query("language") language: FreemiusSupportedCheckoutLanguages,
+  ) {
+    const parameters = {
+      planId,
+      userEmail,
+      productId,
+      readonlyUser,
+      environment,
+      language,
+    };
+    return this.freemiusService.getCheckoutLink(parameters);
   }
 }
